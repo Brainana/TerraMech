994f0a0780ea2901b84769d3ee7ff6c7baec703a
Brainana <63419397+Brainana@users.noreply.github.com>
2020-04-09 19:00:04 -0400

    Add files via upload

--- terraMech.py
+++ terraMech.py
@@ -7,19 +7,23 @@ import picamera
 import time
 import glob
 import os
+import subprocess
 # import servo
 from gpiozero import Servo
 from time import sleep
 
 cameraEnable = True;
 motorEnable = True;
-tensorflowEnable = False;
+tensorflowEnable = True;
 servoEnable = True;
 speakerEnable = True;
 
 camera = picamera.PiCamera()
 
 #servo = Servo(27)
+
+subprocess.run(["aplay", "start.wav"])
+time.sleep(0.5)
 
 for f in glob.glob("lawn*.jpg"):
 	os.remove(f)
@@ -31,10 +35,13 @@ if tensorflowEnable:
 	prediction.setModelPath("resnet.hdf5")
 	prediction.setJsonPath("weeds_model_class.json")
 	print("loading Tensorflow weed model")
+	subprocess.run(["aplay", "model.wav"])
 	modelLoadStart=datetime.datetime.now()
 	prediction.loadModel(num_objects=9)
 	modelLoadEnd=datetime.datetime.now()
 	print("Finished loading model in " + str(modelLoadEnd-modelLoadStart))
+	subprocess.run(["aplay", "modelDone.wav"])
+	time.sleep(0.5)
 
 
 def motorInit():
@@ -68,7 +75,8 @@ servo = Servo(27)
 
 for x in range(0,5):
 	if motorEnable:
-		print ("Robot is  moveing forward")
+		print ("Robot is  moving forward")
+		subprocess.run(["aplay", "move.wav"])
 		motorForward(0.625)
 
 	# Pause the robot so the camera can take a steady picture
@@ -76,28 +84,38 @@ for x in range(0,5):
 	
 	weedFound = False
 
-	imageName = 'lawn' + str(x) + '.jpg'
-	print ("Taking a picture of the lawn.  Image name is " + imageName)
 	if cameraEnable:
+		imageName = 'lawn' + str(x) + '.jpg'
+		print ("Taking a picture of the lawn.  Image name is " + imageName)
+		subprocess.run(["aplay", "picture.wav"])
+		time.sleep(0.5)
 		camera.capture(imageName)
 		if tensorflowEnable:
+			subprocess.run(["aplay", "analyze.wav"])
 			startImgDetection=datetime.datetime.now()
 			predictions, probabilities = prediction.predictImage(imageName, result_count=3)
 			endImgDetection=datetime.datetime.now()
 			imgDetectionDelta=endImgDetection-startImgDetection
+			time.sleep(0.5)
 			print("Image detected in " + str(imgDetectionDelta))
 			for eachPrediction, eachProbability in zip(predictions, probabilities):
 				print(eachPrediction , " : " , eachProbability)
 				if eachPrediction == "Chinee apple":
 					os.rename(imageName, 'lawn' + str(x) + "_Chinee_apple.jpg")
 					weedFound = True
+					subprocess.run(["aplay", "weed.wav"])		
+					time.sleep(1)
 					if servoEnable:
 						servoSpin(0.8);
 					break
 
 			if weedFound == False: 		
 				os.rename(imageName, 'lawn' + str(x) + "_grass.jpg")
+				subprocess.run(["aplay", "grass.wav"])
+				time.sleep(0.5)
 
 camera.close()
  
 gpio.cleanup()
+
+subprocess.run(["aplay", "end.wav"])
